a343335c491be7b48f2c815aad01fd0b231293f2
Roman Hlushko <roman.hlushko@datarobot.com>
2025-10-22 19:31:54 +0200

    Added a test to check FS events

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,33 @@
 import asyncio
+from contextlib import asynccontextmanager
+
 import async_timeout
-from typing import AsyncContextManager
+from typing import AsyncContextManager, AsyncGenerator, Self
 
 from notifykit import NotifierT, Event
 
 
 class EventCollector:
-    def __init__(self, notifier: NotifierT):
-        self._notifier = notifier
+    def __init__(self, ):
         self._events: list[Event] = []
         self._notifier_task: asyncio.Task | None = None
 
         self._waiters: set[tuple[int, asyncio.Event]] = set()
 
-    async def collect(self) -> "AsyncContextManager[EventCollector]":
+    @property
+    def events(self) -> list[Event]:
+        return self._events
+
+    @asynccontextmanager
+    async def collect(self, notifier: NotifierT) -> AsyncGenerator[Self, None]:
         if self._notifier_task is not None:
             raise RuntimeError("EventCollector is already running.")
 
-        self._notifier_task = asyncio.create_task(self._collect_events())
+        self._notifier_task = asyncio.create_task(self._collect_events(notifier))
 
         yield self
 
-        self._notifier.stop()
+        notifier.stop()
 
         if self._notifier_task:
             self._notifier_task.cancel()
@@ -45,8 +51,8 @@ class EventCollector:
 
         self._waiters.remove(waiter_id)
 
-    async def _collect_events(self) -> None:
-        async for event in self._notifier:
+    async def _collect_events(self, notifier: NotifierT) -> None:
+        async for event in notifier:
             self._events.extend(event)
             self._wakeup_waiters()
 
--- tests/test_events_create.py
+++ tests/test_events_create.py
@@ -1,20 +1,31 @@
+import asyncio
 import tempfile
 from pathlib import Path
 
-from notifykit import Notifier
+from notifykit import Notifier, NotifierT
+from tests.conftest import EventCollector
 
 
 async def test__events__create_file() -> None:
     files_to_create = 3
     tmp_dir = Path(tempfile.mkdtemp())
 
-    notifier = Notifier(debounce_ms=100, tick_ms=10, debug=False)
+    await asyncio.sleep(0.1)  # avoid catching directory creation event
+
+    notifier: NotifierT = Notifier(debounce_ms=100, tick_ms=10, debug=False)
     await notifier.watch([tmp_dir])
 
-    expected_paths = []
+    collector = EventCollector()
 
-    for idx in range(files_to_create):
-        file_path = tmp_dir / f'lorem_{idx}.txt'
-        file_path.write_text("new lorem ipsum")
+    async with collector.collect(notifier) as c:
+        expected_paths = []
 
-        expected_paths.append(str(file_path))
+        for idx in range(files_to_create):
+            file_path = tmp_dir / f'lorem_{idx}.txt'
+            file_path.write_text("new lorem ipsum")
+
+            expected_paths.append(str(file_path))
+
+        await c.wait_for_events(3, timeout=3)
+
+    assert len(collector.events) == files_to_create, collector.events
